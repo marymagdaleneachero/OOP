3f53e01e0c6580f7601de7a2a62d9bc1004e657c
Mary Magdalene Achero <marymagdaleneachero@gmail.com>
2025-11-23 07:33:57 +0300

    Add destructor to print farewell message when object is deleted

--- constructors_and_deconstructors.py
+++ constructors_and_deconstructors.py
@@ -3,4 +3,6 @@ class Person:
         self.name = name
         self.age - age
 
-    
+    def __del__(self):
+        # Destructor: runs when the object is about to be deleted
+        print(f"Goodbye {self.name}! The Person object is being deleted.")
