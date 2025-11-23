f28d96c020e0f2b2c73f6b09207af7135c853d58
Mary Magdalene Achero <marymagdaleneachero@gmail.com>
2025-11-23 07:58:19 +0300

    Add Animal class with eat and sleep methods then created a dog subclass to inherit from animal

---
+++ class_inheriance.py
@@ -0,0 +1,11 @@
+class Animal:
+    def eat():
+        print("The animal is eating.")
+
+
+    def sleep():
+        print("The animal is sleeping.")
+
+class Dog(Animal):
+    def bark():
+        print("The dog is barking.")
