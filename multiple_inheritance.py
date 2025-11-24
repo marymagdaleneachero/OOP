7983c390812a57d35b80bd7365682aba89010be4
Mary Magdalene Achero <marymagdaleneachero@gmail.com>
2025-11-24 22:31:57 +0300

    Created subclass to inherit from both bird and mammal

--- multiple_inheritance.py
+++ multiple_inheritance.py
@@ -1,6 +1,13 @@
 class Bird:
-    def fly():
-        pass
+    def fly(self):
+        print("This bird is flying")
 class Mammal:
-    def run():
-        pass
+    def run(self):
+        print("This mammal is running")
+
+class Bat(Bird, Mammal):
+    pass
+
+bat = Bat()
+bat.fly()
+bat.run()
