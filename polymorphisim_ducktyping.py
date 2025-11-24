2f87c33a0d00e253ff9da5b50e1becb9ccb48db4
Mary Magdalene Achero <marymagdaleneachero@gmail.com>
2025-11-24 22:51:00 +0300

    Added function to call the meyhods polymorphically

--- polymorphisim_ducktyping.py
+++ polymorphisim_ducktyping.py
@@ -1,12 +1,19 @@
 class Dog:
-    def make_sound():
+    def make_sound(self):
         print("WOOF")
 class Cat:
-    def make_sound():
+    def make_sound(self):
         print("MEOW")
 
 class Bird:
-    def make_sound():
+    def make_sound(self):
         print("WHOOSH")
+
+def let_them_speak(animals):
+    for animal in animals:
+        animal.make_sound()
+
+animals = [Dog(), Cat(), Bird()]
+let_them_speak(animals)
 
 
