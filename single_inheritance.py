f36109a4b8fb88468c5dcfeb8629e096b303a3ed
Mary Magdalene Achero <marymagdaleneachero@gmail.com>
2025-11-24 22:18:00 +0300

    Created a subclass to inherit from shape class

--- single_inheritance.py
+++ single_inheritance.py
@@ -1,2 +1,11 @@
 class Shape:
     def calculate_area():
+        return "Area not defined for generic shape."
+
+class Rectangle(Shape):
+    def __init__(self,length,width):
+        self.length = length
+        self.width = width
+        
+    def calculate_area(self):
+        return self.length * self.width
