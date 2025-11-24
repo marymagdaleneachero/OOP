7ec2aa78549f10da9a3fbd9ade90e9be6fb7bb63
Mary Magdalene Achero <marymagdaleneachero@gmail.com>
2025-11-24 22:37:09 +0300

    Created three classes and implemented a method for all

---
+++ polymorphisim_ducktyping.py
@@ -0,0 +1,12 @@
+class Dog:
+    def make_sound():
+        print("WOOF")
+class Cat:
+    def make_sound():
+        print("MEOW")
+
+class Bird:
+    def make_sound():
+        print("WHOOSH")
+
+
