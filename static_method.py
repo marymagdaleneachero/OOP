07b5196fa48da24660cedbdd19bc6ba8606f55af
Mary Magdalene Achero <marymagdaleneachero@gmail.com>
2025-11-25 12:57:04 +0300

    Created a claculator class with static methods to addition and multiplication

---
+++ static_method.py
@@ -0,0 +1,13 @@
+class Calculator:
+
+    @staticmethod
+    def add(a,b):
+        return a + b
+
+    @staticmethod
+    def multiply(a,b):
+        return a * b
+    
+
+print(Calculator.add(5, 3))       
+print(Calculator.multiply(5, 3)) 
